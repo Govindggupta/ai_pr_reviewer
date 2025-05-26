919717e23b0ddc83d711b22f2bf3d6cd4569c85c
Govind Gupta <govind12771277@gmail.com>
2025-05-26 12:02:50 +0530

    backend_setup

--- src/app/api/pr-commits/route.tsx
+++ src/app/api/pr-commits/route.tsx
@@ -4,18 +4,20 @@ import { Octokit } from "octokit";
 export async function GET() {
   const octokit = new Octokit();
   const response = await octokit.request(
-    "GET /repos/{owner}/{repo}/pulls/{pull_number}",
+    "GET /repos/{owner}/{repo}/pulls/{pull_number}/commits",
     {
       owner: "TuringLang",
-      repo: "turinglang.github.io",
-      pull_number: 123,
+      repo: "DynamicPPL.jl",
+      pull_number: 346,
       headers: {
         "X-GitHub-Api-Version": "2022-11-28",
       },
     }
   );
+
+  
+
   return NextResponse.json({
-    response
+    response,
   });
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import axios from "axios";
+export default async function Home() {
+
+  const res = await axios.get("http://localhost:3000/api/test");
+
+  
+
+
   return (
     <>
       <div>hello world</div>
+      <div>{JSON.stringify(res.data.response.url)}</div>
+
+      <a href={res.data.response.url}>{res.data.response.url}</a>
+
+      
+      
     </>
   );
 }
