b3ff2558373b6478243e18583ed89f9827d18b67
Govindggupta <govind12771277@gmail.com>
2025-06-10 17:36:25 +0000

    landing and chat page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,8 @@
+export default function Chat() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">Chat</div>
+      
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Page() {
-    return (
-        <div>
-            <h1>New start</h1>
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex items-center justify-center">
+          <h1>AI PR Reviewer</h1>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => router.push("/chat")}
+          >
+            Start Reviewing
+          </button>
         </div>
-    )
+      </div>
+    </>
+  );
 }
