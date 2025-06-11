c8feadb5a9eb35c75c8bd3c58f46e2004a06d777
Govindggupta <govind12771277@gmail.com>
2025-06-11 18:24:46 +0000

    check

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,7 +2,9 @@ export default function Chat() {
   return (
     <>
       <div className="flex justify-center items-center h-screen">Chat</div>
-      
+      <div>
+        hello world
+      </div>
     </>
   );
 }
